87b21a47a09c4fd5d26c6d55516262310a12f7e5
Darren Meehan <hi@drn.ie>
2022-05-29 10:12:24 +0100

    Add pihole VM

--- main.tf
+++ main.tf
@@ -93,6 +93,49 @@ resource "proxmox_vm_qemu" "dev_server" {
   bootdisk = "scsi0"
   disk {
     slot     = 0
+    # FIXME Change to 100G
+    size     = "10G"
+    type     = "scsi"
+    storage  = "local-lvm"
+    iothread = 1
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+  # not sure exactly what this is for. presumably something about MAC addresses and ignore network changes during the life of the VM
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  ipconfig0 = "ip=dhcp"
+  # sshkeys set using variables. the variable contains the text of the key.
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
+
+
+
+
+resource "proxmox_vm_qemu" "pihole" {
+  desc = "Network-wide Ad Blocking"
+  count       = 1
+  name        = "pihole-${count.index + 1}"
+  target_node = "pve"
+  clone = "ubuntu-2204-template-9004"
+  agent    = 1
+  os_type  = "cloud-init"
+  cores    = 2
+  sockets  = 1
+  cpu      = "host"
+  memory   = 2048
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  disk {
+    slot     = 0
     size     = "10G"
     type     = "scsi"
     storage  = "local-lvm"
