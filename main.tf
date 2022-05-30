f435ddaf31cf5e992f78e8b4ca061f847862b78e
Darren Meehan <hi@drn.ie>
2022-05-30 18:09:05 +0100

    Use latest image

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "proxmox_vm_qemu" "certs_server" {
   count       = 1
   name        = "certs-${count.index + 1}"
   target_node = "pve"
-  clone = "ubuntu-2204-template"
+  clone = "ubuntu-2204-template-9005"
   agent    = 1
   os_type  = "cloud-init"
   cores    = 2
@@ -42,7 +42,7 @@ resource "proxmox_vm_qemu" "octoprint_server" {
   count       = 1
   name        = "octoprint"
   target_node = "pve"
-  clone = "ubuntu-2204-template"
+  clone = "ubuntu-2204-template-9005"
   agent    = 1
   os_type  = "cloud-init"
   cores    = 2
@@ -76,13 +76,12 @@ resource "proxmox_vm_qemu" "octoprint_server" {
   EOF
 }
 
-
 resource "proxmox_vm_qemu" "dev_server" {
   desc = "Rust development machine"
   count       = 1
   name        = "dev-${count.index + 1}"
   target_node = "pve"
-  clone = "ubuntu-2204-template"
+  clone = "ubuntu-2204-template-9005"
   agent    = 1
   os_type  = "cloud-init"
   cores    = 4
@@ -117,15 +116,12 @@ resource "proxmox_vm_qemu" "dev_server" {
   EOF
 }
 
-
-
-
 resource "proxmox_vm_qemu" "pihole" {
   desc = "Network-wide Ad Blocking"
   count       = 1
   name        = "pihole-${count.index + 1}"
   target_node = "pve"
-  clone = "ubuntu-2204-template-9004"
+  clone = "ubuntu-2204-template-9005"
   agent    = 1
   os_type  = "cloud-init"
   cores    = 2
@@ -159,13 +155,12 @@ resource "proxmox_vm_qemu" "pihole" {
   EOF
 }
 
-
 resource "proxmox_vm_qemu" "photos" {
   desc = "Photo Manager"
   count       = 1
   name        = "photos-${count.index + 1}"
   target_node = "pve"
-  clone = "ubuntu-2204-template-9004"
+  clone = "ubuntu-2204-template-9005"
   agent    = 1
   os_type  = "cloud-init"
   cores    = 2
@@ -177,6 +172,45 @@ resource "proxmox_vm_qemu" "photos" {
   disk {
     slot     = 0
     size     = "50G"
+    type     = "scsi"
+    storage  = "local-lvm"
+    iothread = 1
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+  # not sure exactly what this is for. presumably something about MAC addresses and ignore network changes during the life of the VM
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+  ipconfig0 = "ip=dhcp"
+  # sshkeys set using variables. the variable contains the text of the key.
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
+
+resource "proxmox_vm_qemu" "k0s" {
+  desc = "Container home, not actually k0s"
+  count       = 1
+  name        = "k0s-${count.index + 1}"
+  target_node = "pve"
+  clone = "ubuntu-2204-template-9005"
+  agent    = 1
+  os_type  = "cloud-init"
+  cores    = 2
+  sockets  = 1
+  cpu      = "host"
+  memory   = 2048
+  scsihw   = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  disk {
+    slot     = 0
+    size     = "20G"
     type     = "scsi"
     storage  = "local-lvm"
     iothread = 1
